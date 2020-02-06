a3d91719f9a3154180c4d414f92dc25f4aab824d
Can Yang <cyang@kth.se>
2020-02-06 11:10:54 +0100

    :hammer: Move SPDLOG macro to cmake file

--- src/debug.h
+++ src/debug.h
@@ -9,8 +9,6 @@
 #ifndef MM_DEBUG_HPP
 #define MM_DEBUG_HPP
 
-#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
-
 #include "spdlog/spdlog.h"
 // must be included for custom operator
 #include "spdlog/fmt/ostr.h"
