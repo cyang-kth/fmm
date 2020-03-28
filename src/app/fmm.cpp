40c64b26e4571fc63836cbd3761258b6ad358a59
Can Yang <cyang@kth.se>
2020-03-28 14:00:37 +0100

    Remove omp app

--- src/app/fmm.cpp
+++ src/app/fmm.cpp
@@ -15,105 +15,105 @@ using namespace MM::IO;
 
 void run(int argc, char **argv)
 {
-  if (argc<2)
-  {
-    std::cout<<"A configuration file is given in the example folder\n";
-    std::cout<<"Run `fmm config.xml` or with arguments\n";
-    FMM_Config::print_help();
-  } else {
-    if (argc==2){
-      std::string first_arg(argv[1]);
-      if (first_arg=="--help"||first_arg=="-h"){
-        FMM_Config::print_help();
-        return;
-      }
-    }
-    FMM_Config config(argc,argv);
-    if (!config.validate_mm())
-    {
-      SPDLOG_CRITICAL("Validation fail, program stop");
-      return;
-    };
-    config.print();
-    std::chrono::steady_clock::time_point begin =
-      std::chrono::steady_clock::now();
-    Network network(
-      config.network_file,config.network_id,
-      config.network_source,config.network_target);
-    network.build_rtree_index();
-    int multiplier = network.get_node_count();
-    if (multiplier==0) multiplier = 50000;
-    UBODT *ubodt=nullptr;
-    if (config.binary_flag==1) {
-      ubodt = read_ubodt_binary(config.ubodt_file,multiplier);
-    } else {
-      ubodt = read_ubodt_csv(config.ubodt_file,multiplier);
-    }
-    if (!config.delta_defined) {
-      config.delta = ubodt->get_delta();
-      SPDLOG_INFO("Delta inferred from ubodt as {}",config.delta);
-    }
-    GPSReader gps_reader(config);
-    ResultConfig result_config = config.get_result_config();
-    ResultWriter rw(config.result_file,network,result_config);
-    int progress=0;
-    int points_matched=0;
-    int total_points=0;
-    int num_trajectories = gps_reader.get_trajectory_number();
-    int step_size = 1;
-    if (num_trajectories<0) {
-      step_size = 1000;
-      SPDLOG_INFO("Progress report step {}",step_size);
-    } else {
-      step_size = num_trajectories /10;
-      if (step_size<10) step_size=10;
-      SPDLOG_INFO("Total trajectory number {}",num_trajectories);
-      SPDLOG_INFO("Progress report step {}",step_size);
-    }
-    std::chrono::steady_clock::time_point corrected_begin =
-      std::chrono::steady_clock::now();
-    while (gps_reader.has_next_trajectory())
-    {
-      Trajectory trajectory = gps_reader.read_next_trajectory();
-      int points_in_tr = trajectory.geom.getNumPoints();
-      // Candidate search
-      Traj_Candidates traj_candidates = network.search_tr_cs_knn(
-        trajectory,config.k,config.radius,config.gps_error);
-      TransitionGraph tg(
-        &traj_candidates,trajectory.geom,*ubodt,config.delta);
-      // Optimal path inference
-      O_Path o_path = tg.viterbi(config.penalty_factor);
-      T_Path t_path = ubodt->construct_traversed_path(o_path,network);
-      LineString m_geom;
-      if (result_config.write_mgeom) {
-        m_geom = network.complete_path_to_geometry(o_path,t_path.cpath);
-      }
-      rw.write_result(trajectory.id,trajectory.geom,o_path,t_path,m_geom);
-      // update statistics
-      total_points+=points_in_tr;
-      if (!t_path.cpath.empty()) points_matched+=points_in_tr;
-      ++progress;
-      if (progress%step_size==0)
-        SPDLOG_INFO("Progress {}",progress);
-    }
-    SPDLOG_INFO("Progress {}",progress);
-    SPDLOG_TRACE("MM process finished");
-    std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
-    double time_spent =
-      std::chrono::duration_cast<std::chrono::milliseconds>
-        (end - begin).count()/1000.;
-    double time_spent_exclude_input =
-      std::chrono::duration_cast<std::chrono::milliseconds>
-        (end - corrected_begin).count()/1000.;
-    SPDLOG_INFO("Time takes {}, excluding input takes",
-                time_spent, time_spent_exclude_input);
-    SPDLOG_INFO("Total points {} matched {}",total_points,points_matched);
-    SPDLOG_INFO("Matched percentage: {}",points_matched/(double)total_points);
-    SPDLOG_INFO("Point match speed: {} pt/s (excluding input {} pt/s)",
-                points_matched/time_spent,
-                points_matched/time_spent_exclude_input);
-    delete ubodt;
+
+  UTIL::TimePoint start_time = std::chrono::steady_clock::now();
+  std::shared_ptr<MMInterface> mm_model;
+  switch (config_.algorithm_id) {
+    case (MM_ALGOR_TAG::CWRMM):
+      SPDLOG_INFO("Create CWRMM model")
+      mm_model = std::make_shared<CWRMM>(network_,ng_);
+      break;
+    case (MM_ALGOR_TAG::FCDMM):
+      SPDLOG_INFO("Create FCDMM model")
+      mm_model = std::make_shared<FCDMM>(network_,ng_);
+      break;
+    case (MM_ALGOR_TAG::STMATCH):
+      SPDLOG_INFO("Create STMATCH model")
+      mm_model = std::make_shared<STMATCH>(network_,ng_);
+      break;
+    default:
+      SPDLOG_CRITICAL("MM Algorithm not found")
+      break;
   }
+  if (!config_.mm_log_file.empty()){
+    SPDLOG_INFO("Write log of mm to file {}", config_.mm_log_file)
+    auto mm_logger = spdlog::basic_logger_mt("mm_logger",
+                                             config_.mm_log_file,true);
+    mm_logger->set_level(static_cast<spdlog::level::level_enum>
+                         (config_.mm_log_level));
+    //mm_logger->set_pattern("[%^%l%$][%s:%-3#] %v");
+    mm_logger->set_pattern("%v");
+    mm_model->set_logger(mm_logger);
+  }
+  MMConfig mm_config = config_.get_mm_config();
+  // Create trajectory reader and result writer
+  std::shared_ptr<IO::TemporalGPSReader> reader;
+  if (config_.gps_point){
+    reader = std::make_shared<IO::CSVTemporalPointReader>(
+        config_.gps_file,config_.gps_id,
+        config_.gps_x,config_.gps_y, config_.gps_timestamp
+        );
+  } else {
+    reader = std::make_shared<IO::CSVTemporalTrajectoryReader>(
+        config_.gps_file,config_.gps_id,
+        config_.gps_geom,config_.gps_timestamp
+    );
+  }
+  OutputConfig result_config = config_.get_result_config();
+  IO::CSVMatchResultWriter writer(config_.result_file,result_config);
+  int progress=0;
+  int points_matched=0;
+  int total_points=0;
+  int step_size = 100;
+  if (config_.step>0) step_size = config_.step;
+  SPDLOG_INFO("Progress report step {}",step_size)
+  UTIL::TimePoint corrected_begin = std::chrono::steady_clock::now();
+  SPDLOG_INFO("Start to match trajectories")
+  // The header is moved to constructor of result writer
+  // rw.write_header();
+  while (reader->has_next_feature())
+  {
+    if (progress%step_size==0) {
+      SPDLOG_INFO("Progress {}",progress)
+    }
+    if (reader->has_time_stamp()){
+      TemporalTrajectory traj = reader->read_next_temporal_trajectory();
+      int points_in_tr = traj.geom.get_num_points();
+      MM::MatchResult result = mm_model->match_temporal_traj(
+          traj, mm_config);
+      writer.write_result(result);
+      if (!result.cpath.empty()) {
+        points_matched+=points_in_tr;
+      }
+      total_points+=points_in_tr;
+    } else {
+      Trajectory traj = reader->read_next_trajectory();
+      int points_in_tr = traj.geom.get_num_points();
+      MM::MatchResult result = mm_model->match_traj(
+          traj, mm_config);
+      writer.write_result(result);
+      if (!result.cpath.empty()) {
+        points_matched+=points_in_tr;
+      }
+      total_points+=points_in_tr;
+    }
+    ++progress;
+  }
+  SPDLOG_INFO("MM process finished")
+  UTIL::TimePoint end_time = std::chrono::steady_clock::now();
+  double time_spent = std::chrono::duration_cast<std::chrono::milliseconds>(
+      end_time - start_time).count()/1000.;
+  double time_spent_exclude_input = std::chrono::duration_cast<
+      std::chrono::milliseconds>(end_time - corrected_begin).count()/1000.;
+  SPDLOG_INFO("Time takes {}",time_spent)
+  SPDLOG_INFO("Time takes excluding input {}",time_spent_exclude_input)
+  SPDLOG_INFO("Finish map match total points {} matched {}",
+              total_points,points_matched)
+  SPDLOG_INFO("Matched percentage: {}",points_matched/(double)total_points)
+  SPDLOG_INFO("Point match speed: {}",points_matched/time_spent)
+  SPDLOG_INFO("Point match speed (excluding input): {}",
+              points_matched/time_spent_exclude_input)
+  SPDLOG_INFO("Time takes {}",time_spent)
 };
 
 int main(int argc, char **argv){
