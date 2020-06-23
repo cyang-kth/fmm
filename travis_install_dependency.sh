3e9c39dd17385416b164be4ace2e9ad1b5551ca4
Can Yang <cyang@kth.se>
2020-06-23 13:50:20 +0200

    Ignore brew install error on mac

--- travis_install_dependency.sh
+++ travis_install_dependency.sh
@@ -2,7 +2,7 @@
 
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
   brew update;
-  brew install boost gdal libomp expat swig;
+  brew install boost gdal libomp expat swig || true;
 else
   sudo add-apt-repository -y ppa:ubuntugis/ppa;
   sudo apt-get -q update;
