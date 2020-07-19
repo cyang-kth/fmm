bbe8c5d85479b8bd48fbf9c156a8b6d89a360649
Can Yang <cyang@kth.se>
2020-07-19 14:06:44 +0200

    Add basic functions

--- src/network/topology.hpp
+++ src/network/topology.hpp
@@ -19,8 +19,55 @@ struct EdgeIntersectionData{
   double offset;
 };
 
+class TopologyGeneratorConfig
+{
+ public:
+  /**
+   * Constructor of the configuration from command line arguments.
+   * The argument data are fetched from the main function directly.
+   *
+   * @param argc number of arguments
+   * @param argv raw argument data
+   *
+   */
+  TopologyGeneratorConfig(int argc, char **argv);
+  /**
+   * Validate the configuration
+   * @return true if valid
+   */
+  inline bool validate() const;
+  /**
+   * Print configuration data
+   */
+  inline void print() const;
+  /**
+   * Print help information
+   */
+  static inline void print_help();
+  std::string output_folder; /**< UBODT file name */
+  CONFIG::NetworkConfig network_config;/**< Network data configuraiton */
+  double tolerance;
+  bool split_intersection = false;
+  bool help_specified = false;  /**< Help is specified or not */
+  int log_level = 2;  /**< log level, 0-trace,1-debug,2-info,
+                          3-warn,4-err,5-critical,6-off */
+  int step = 100; /**< progress report step */
+}; // TopologyGeneratorConfig
+
 class TopologyGenerator{
 public:
+  TopologyGenerator(const TopologyGeneratorConfig &config);
+  inline void run(){
+    std::vector<FMM::CORE::LineString> raw_data = read_geom_data(
+      config_.network_config.network_file);
+    std::vector<Edge> topology_data  = create_topology(
+      raw_data, config_.tolerance, config_.split_intersection);
+    int status = export_result(topology_data,config_.output_folder);
+  };
+  static int export_result(const std::vector<Edge> &topology_data,
+    const std::string &output_folder){
+
+  };
   static std::vector<FMM::CORE::LineString> read_geom_data(
     const std::string &network_file){
 
@@ -30,7 +77,11 @@ public:
     bool split_intersection = false) {
 
   };
+private:
+  const FMMAppConfig &config_;
 };
+
+
 
 }; // NETWORK
 
